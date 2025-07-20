b9d8e3fa640bdd86d4f48df5ad761ea2c4476692
mukesh-mvm <mukeshkushwaha.mvmbs@gmail.com>
2025-07-20 18:38:29 +0530

    ljfsgjfd

--- index.js
+++ index.js
@@ -168,6 +168,24 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 
+
+// demo testimomial
+const swiper = new Swiper('.testimonial-section-swiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    1024: { slidesPerView: 3 },
+    768: { slidesPerView: 2 },
+    0: { slidesPerView: 1 },
+  }
+});
+
+
 // testinomia
 
 
